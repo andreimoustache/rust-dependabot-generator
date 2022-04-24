fbf1b70b8eb14d16ab689d2ed8d1a5a2668646fe
Andrei Mustata <hello@amoustache.net>
2022-04-24 17:17:39 +0200

    Switch `str`s to `String`

--- src/main.rs
+++ src/main.rs
@@ -9,38 +9,38 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn is_target(mapping: &HashMap<&str, &str>, entry: &DirEntry) -> bool {
+fn is_target(mapping: &HashMap<String, String>, entry: &DirEntry) -> bool {
     return entry
         .file_name()
         .to_str()
-        .map(|s| mapping.contains_key(s))
+        .map(|s| mapping.contains_key(&s.to_string()))
         .unwrap_or(false);
 }
 
-fn is_ignored(ignored_dirs: &HashSet<&str>, entry: &DirEntry) -> bool {
+fn is_ignored(ignored_dirs: &HashSet<String>, entry: &DirEntry) -> bool {
     return entry
         .file_name()
         .to_str()
-        .map(|s| ignored_dirs.contains(s))
+        .map(|s| ignored_dirs.contains(&s.to_string()))
         .unwrap_or(false);
 }
 
 fn find_targets(
-    mapping: HashMap<&str, &str>,
-    ignored_dirs: HashSet<&str>,
+    mapping: HashMap<String, String>,
+    ignored_dirs: HashSet<String>,
     walk: WalkDir,
 ) -> HashSet<String> {
     let mut found: HashSet<String> = HashSet::new();
 
-    for entry in walk
+    for found_target in walk
         .follow_links(true)
         .into_iter()
         .filter_entry(|entry| !is_ignored(&ignored_dirs, entry))
         .filter_map(|entry| entry.ok())
         .filter(|entry| is_target(&mapping, entry))
+        .filter_map(|entry| entry.file_name().to_str().map(|s| s.to_string()))
     {
-        let found_target = entry.file_name().to_str().unwrap();
-        let found_for = mapping.get(found_target).unwrap();
+        let found_for = mapping.get(&found_target).unwrap();
         found.insert(found_for.to_string());
     }
 
@@ -51,17 +51,20 @@ fn main() {
     let args = Cli::parse();
     println!("Scanning directory {}.", args.path.to_str().unwrap());
 
-    let ignored_dirs = HashSet::from([".git", "target"]);
-    let mapping = HashMap::from([
-        ("package.json", "npm"),
-        ("package-lock.json", "npm"),
-        ("yarn.lock", "npm"),
-        ("Dockerfile", "docker"),
-        ("Cargo.toml", "cargo"),
-        ("requirements.txt", "pip"),
-        ("pyproject.toml", "pip"),
-        ("poetry.lock", "pip"),
-    ]);
+    let ignored_dirs = HashSet::from([".git", "target"].map(|s| s.to_string()));
+    let mapping = HashMap::from(
+        [
+            ("package.json", "npm"),
+            ("package-lock.json", "npm"),
+            ("yarn.lock", "npm"),
+            ("Dockerfile", "docker"),
+            ("Cargo.toml", "cargo"),
+            ("requirements.txt", "pip"),
+            ("pyproject.toml", "pip"),
+            ("poetry.lock", "pip"),
+        ]
+        .map(|p| (p.0.to_string(), p.1.to_string())),
+    );
 
     let walk_dir = WalkDir::new(args.path);
     let found = find_targets(mapping, ignored_dirs, walk_dir);
@@ -84,7 +87,7 @@ mod tests {
 
     #[test]
     fn is_ignored_test() {
-        let ignored = HashSet::from(["README.md"]);
+        let ignored = HashSet::from([String::from("README.md")]);
         for entry in WalkDir::new("README.md")
             .into_iter()
             .filter_map(|entry| entry.ok())
